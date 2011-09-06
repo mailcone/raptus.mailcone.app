0847de930f91f13d243d69584f259de7cd4eca31
samuel.riolo <samuel.riolo@googlemail.com>
2011-09-06 20:33:45 +0000

    create special navigation directive for automatically locate and display the menu item

--- raptus/mailcone/app/app.py
+++ raptus/mailcone/app/app.py
@@ -4,11 +4,9 @@ import grok, os
 #from zope.app.security.interfaces import IAuthentication
 
 from raptus.mailcone.core.interfaces import IMailcone
-from raptus.mailcone.customers.interfaces import ICustomersContainerHolder
 
 
 
 class Mailcone(grok.Application, grok.Container):
-    grok.implements(IMailcone,
-                    ICustomersContainerHolder)
+    grok.implements(IMailcone)
     #grok.local_utility(PluggableAuthentication, provides=IAuthentication, setup=auth.setup_authentication)
