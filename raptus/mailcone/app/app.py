71c8019ef4af8c999f16e73d68288ebb5e408cca
samuelriolo <samuelriolo@2114add4-67ca-9ca6-10be-5e31bf341269>
2011-09-09 01:30:23 +0000

    implement zope catalog, lot of work for javascript, create base classes for formlib, work on js-datatable

--- raptus/mailcone/app/app.py
+++ raptus/mailcone/app/app.py
@@ -3,10 +3,17 @@ import grok, os
 #from zope.app.authentication.authentication import PluggableAuthentication
 #from zope.app.security.interfaces import IAuthentication
 
-from raptus.mailcone.core.interfaces import IMailcone
+from hurry.query.query import Query
+from hurry.query.interfaces import IQuery
 
+from z3c.relationfield import RelationCatalog
+from zc.relation.interfaces import ICatalog
+
+from raptus.mailcone.core.interfaces import IMailcone
 
 
 class Mailcone(grok.Application, grok.Container):
     grok.implements(IMailcone)
     #grok.local_utility(PluggableAuthentication, provides=IAuthentication, setup=auth.setup_authentication)
+    grok.local_utility(Query, provides=IQuery)
+    grok.local_utility(RelationCatalog, provides=ICatalog)
