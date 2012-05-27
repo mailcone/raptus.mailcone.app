064774918a38099395082eeb6f2db344e4fc2e4b
samuelriolo <samuelriolo@2114add4-67ca-9ca6-10be-5e31bf341269>
2012-05-27 01:36:47 +0000

    add ldap authentication

--- raptus/mailcone/app/app.py
+++ raptus/mailcone/app/app.py
@@ -1,11 +1,12 @@
 import grok, os
 
-#from zope.app.authentication.authentication import PluggableAuthentication
-#from zope.app.security.interfaces import IAuthentication
+from zope.app.authentication.authentication import PluggableAuthentication
+from zope.app.security.interfaces import IAuthentication
 
 from hurry.query.query import Query
 from hurry.query.interfaces import IQuery
 
+from raptus.mailcone.auth.auth import setup_authentication
 from raptus.mailcone.cronjob.contents import CronJobContainer
 
 from raptus.mailcone.core.interfaces import IMailcone
@@ -13,6 +14,6 @@ from raptus.mailcone.core.interfaces import IMailcone
 
 class Mailcone(grok.Application, grok.Container):
     grok.implements(IMailcone)
-    #grok.local_utility(PluggableAuthentication, provides=IAuthentication, setup=auth.setup_authentication)
+    grok.local_utility(PluggableAuthentication, provides=IAuthentication, setup=setup_authentication)
     grok.local_utility(Query, provides=IQuery)
     grok.local_utility(CronJobContainer, name_in_container='cronjobs', public=True)
